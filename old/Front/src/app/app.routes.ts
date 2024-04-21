35838b9acae51d2daddaf0d4015f440605450371
Jeremy-Huleux <huleux.jeremy@gmail.com>
2024-04-21 13:13:33 +0200

    DELETE

--- old/Front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { NotFoundComponent } from './commons/not-found/not-found.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path:'home', component: HomePageComponent },
-    { path: '**', component: NotFoundComponent}
-];
