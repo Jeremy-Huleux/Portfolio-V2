d629915270cb484a567fc249c0c1724ed6cf331c
Jeremy-Huleux <huleux.jeremy@gmail.com>
2024-04-23 09:08:30 +0200

    update

--- Front/src/app/pages/home-page/home-page.component.ts
+++ Front/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,16 @@
-import { Component, ElementRef} from '@angular/core';
+import { Component, ElementRef, OnInit} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons'; 
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import { PlatformService } from '../../services/platform.service';
+import Rellax from 'rellax';
+import { gsap } from "gsap";
+
+
+
+
 
 @Component({
   selector: 'app-home-page',
@@ -14,7 +20,8 @@ import { PlatformService } from '../../services/platform.service';
   imports: [FontAwesomeModule]
 })
 
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  constructor(private platformService: PlatformService) { }
   
   /**
    * Creation des icones avec faBar (font awasome)
@@ -27,6 +34,8 @@ export class HomePageComponent {
   /**
    * Variables
    */
+  rellax!: any;
+  gsap!: any;
   styles: string[] = [
                         '--clr: #d1eb3e; --txt: #fff',
                         '--clr: #3eeb69; --txt: #fff',
@@ -37,9 +46,27 @@ export class HomePageComponent {
                       ]; // doit contenir les variable CSS dynamique : --clr: #081b29; --txt: #fff
   color!: String;
   text!: String;
+  svg!: any;
 
   //TODO: trouver le moyen de savoir dans quel section est la fonction puis mettre le style adapter dynamique 
 
+  gsapisation(){
+    this.svg = document.querySelector(".angular");
+
+    this.gsap = gsap.from(this.svg, { 
+      rotation: 360, 
+      duration: 2, 
+      ease: "bounce.out" 
+    });
+    
+  }
+  //à l'initialisation de la page coté client on initialise le script rellax 
+  ngOnInit(){
+    if(this.platformService.isOnVue()){
+      this.rellax = new Rellax('.rellax');
+      this.gsapisation();
+    }  
+  }
 
 }
   
